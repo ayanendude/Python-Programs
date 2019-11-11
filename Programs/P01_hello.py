10f81eb0786426a376317112b9e707a702177046
ayanendude <ayanendu_de@yahoo.co.uk>
2019-11-10 22:21:46 -0500

    gitignore

--- Programs/P01_hello.py
+++ Programs/P01_hello.py
@@ -7,3 +7,29 @@ def justPrint(text):
 
 if __name__ == '__main__':
     justPrint('Hello')
+
+def main():
+    print ("Hello World!")
+
+if __name__== "__main__":
+    main()
+
+print ("Guru99")
+
+oldstring = 'I like Guru99'
+newstring = oldstring.replace('like', 'love')
+n1 = newstring.replace ('G','H')
+print(newstring)
+print (n1)
+
+string="12345"
+print(''.join(reversed(string)))
+print(":".join("Python"))
+
+word="guru99 career guru99"
+print(word.split('r'))
+
+a=(5,6)
+b=(6,4)
+if (a>b):print("a is bigger")
+else: print("b is bigger")
